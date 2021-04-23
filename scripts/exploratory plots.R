cdde187902be92d8f9dbe740c12391e2bed6218c
Kyle Van Houtan <vanhoutan@users.noreply.github.com>
2021-04-23 09:44:53 -0700

    plot updates figure 1

--- scripts/exploratory plots.R
+++ scripts/exploratory plots.R
@@ -2,12 +2,15 @@
 ## build several plots from raw data csvs and DFs 
 ## Pretty standard stuff really 
 
+
 library(plyr)
 library(dplyr)
 library(data.table)
 library(ggthemes)
 library(ggplot2)
+library(tidyverse)
 library(forcats)
+library(tidyr)
 
 
 themeKV <-theme_few()+
@@ -19,25 +22,28 @@ themeKV <-theme_few()+
         panel.border = element_rect(colour = "black", fill=NA, size=.5),
         legend.title=element_blank(),
         strip.text=element_text(hjust=0))
+        
 
+## make a series of plots on the capture program tagging metadata
+## should be figure 1a-h in the paper
 
-## make a series of plots on the capture program
-## should be figure 1a-hc in the paper
-
-## tags deploymed by year, barplot
+## first plot is the no. tags deployed time series
+## line plot
 metaDF <- read.csv('./data/jws_tag_metadata.csv', header = T)
 ggplot(metaDF, aes(x = YEAR_CAP)) +
   themeKV +
-  geom_point(stat="count",alpha=0.65) +
   geom_line(stat="count",alpha=0.65) +
+  geom_point(stat="count",alpha=0.65, size=4, color="orange") +
   xlab("deployment year") +
   ylab("sharks tagged (no. individuals)") +
   scale_y_continuous(limits = c(0,10),
                      breaks = c(0,2,4,6,8,10)) +
   scale_x_continuous(limits = c(2000,2020),
-                     breaks = c(2000,2005,2010,2015,2020))
+                     breaks = c(2000,2004,2008,2012,2016,2020))
   
-## tags deploymed by numerical month, barplot
+
+## tags deploymed by numerical month, lolli + bar plot
+## bar vers
 metaDF <- read.csv('./data/jws_tag_metadata.csv', header = T)
 ggplot(metaDF, aes(x = MONTH_CAP)) +
   themeKV +
@@ -46,35 +52,129 @@ ggplot(metaDF, aes(x = MONTH_CAP)) +
   ylab("sharks tagged (no. individuals)") +
   scale_x_continuous(limits = c(0,12),
                      breaks = c(2,4,6,8,12))
+## lollipop vers
+metaDF <- read.csv('./data/jws_tag_metadata.csv', header = T)
+metaDF %>%
+group_by(MONTH_CAP) %>% 
+  summarise(count = n()) %>% 
+  ggplot(aes(x = MONTH_CAP, y = count)) +
+  themeKV +
+  geom_segment(aes(x= MONTH_CAP, xend= MONTH_CAP, y=0, yend=count), alpha=0.65) +
+  geom_point(alpha=0.65, size=4, color="orange") +
+  xlab("deployment month") +
+  ylab("sharks tagged (no. individuals)") +
+  scale_y_continuous(limits = c(0,16),
+                     breaks = c(0,4,8,12,16)) +
+  scale_x_continuous(limits = c(1,12),
+                     breaks = c(2,4,6,8,10,12))
 
 
-## tags deploymed by latitude, density plot
+## tags deploymed by latitude
+## density plot
 ggplot(metaDF, aes(x = LAT_REL)) +
   themeKV +
-  geom_density(alpha=0.4) +
+  geom_density(color = "orange", fill = "orange", alpha=0.4) +
   xlab("deployment latitude (°)") +
-  scale_x_continuous(limits = c(27,37),
-                     breaks = c(27,28,29,30,31,32,33,34,35,36,37)) +
+  scale_x_continuous(limits = c(27,37.5),
+                     breaks = c(28,30,32,34,36)) +
   scale_y_reverse()+
-  ## if want flip axes
+   ## flip axes to mimic map + coastline orientation
     coord_flip()
 
 
-## types of tags deployed, bar plot
+## types of tags deployed
+## bar plot
+library(viridis)
 tagDF <- read.csv('./data/jws_tag_types.csv', header = T)
 ggplot(tagDF, aes(x = fct_infreq(TAG_MODEL),fill=TAG_TYPE)) +
   ## ggplot(tagDF, aes(x = TAG_MODEL)) +
   themeKV +
-  geom_bar(stat="count",alpha=0.65) +
+  geom_bar(stat="count",alpha=0.75) +
+  scale_fill_viridis_d(
+    alpha = 1,
+    begin = 0.1,
+    end = 0.75,
+    direction = 1,
+    option = "inferno",
+    aesthetics = "fill"
+  ) +
   xlab("tag model") +
   ylab("sharks tagged (no. individuals)") +
   scale_y_continuous(breaks = c(0,5,10,15,20,25,30))
 
 
+## number of tags deployed per shark
+## donut plot
+metaDF <- read.csv('./data/jws_tag_metadata.csv', header = T)
+metaDF %>%
+  group_by(TAGS_NO) %>% 
+  summarise(count = n()) %>% 
+  ggplot(aes(x = 2, y = count, fill = count)) +
+  geom_bar(stat="identity",color="white",show.legend = FALSE) +
+  coord_polar("y", start = 0) +
+##  geom_text(aes(y = lab.pos, label = paste(percentage,"%", sep = "")), col = "white") +
+  theme_void() +
+  scale_fill_viridis_c(
+    alpha = 0.85,
+    begin = 0.1,
+    end = 0.75,
+    direction = -1,
+    option = "inferno",
+    aesthetics = "fill") +
+  xlim(0.5,2.5) +
+  labs(title = "no. tags deployed per shark",
+       ##       subtitle = "capture operation",
+       caption = "1 + 2")  
   
-  
-  
-  
+
+## capture operation type
+## donut plot
+metaDF <- read.csv('./data/jws_tag_metadata.csv', header = T)
+metaDF %>%
+  group_by(INTERACTION) %>% 
+  summarise(count = n()) %>% 
+  ggplot(aes(x = 2, y = count, fill = count)) +
+  geom_bar(stat="identity",color="white",show.legend = FALSE) +
+  coord_polar("y", start = 0) +
+    theme_void() +
+    scale_fill_viridis_c(
+    alpha = 0.85,
+    begin = 0.1,
+    end = 0.75,
+    direction = -1,
+    option = "inferno",
+    aesthetics = "fill") +
+  xlim(0.5,2.5) +
+  labs(title = "capture operation",
+##       subtitle = "capture operation",
+       caption = "commercial fisheries + research")
+
+
+## capture gear type
+## lollipop
+## cannot reorder y axis in descending, was trying to implement code at this URL
+## https://learn.r-journalism.com/en/visualizing/customizing_charts/customizing-exporting-ggplot2/library(forcats)
+metaDF <- read.csv('./data/jws_tag_metadata.csv', header = T)
+metaDF %>%
+  group_by(CAPTURE_GEAR) %>% 
+  summarise(count = n()) %>% 
+  ggplot(aes(x = count, y=CAPTURE_GEAR)) +
+  themeKV +
+  geom_segment(aes(y=CAPTURE_GEAR,yend= CAPTURE_GEAR, x=0, xend=count), alpha=0.65) +
+  geom_point(alpha=0.65, size=4, color="orange") +
+  xlab("interaction gear") +
+  ylab("sharks tagged (no. individuals)") +
+  scale_x_continuous(limits = c(0,17),
+                     breaks = c(0,4,8,12,16))
+
+
+
+y=fct_reorder(CAPTURE_GEAR, actress_age, desc=TRUE)
+
+## Target fishery 
+## bar plot
+
+
 
 
 
